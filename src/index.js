f549371b53079666c26fb91528b4db01c0aab59c
asteria <189134833+asteriash@users.noreply.github.com>
2025-04-24 20:53:55 +0200

    Added timestamp, no longer saves empty files.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 const textArea = document.querySelector("#textArea");
 const hidingObj = document.querySelector("#hidingObject");
 const showButton = document.querySelector("#showButton");
@@ -11,15 +12,26 @@ function showText(){
   hidingObj.style.display = "none";
 }
 
+function getReadableTimestamp() {
+  const now = new Date();
+  return now.toLocaleString('sv-SE').replace(/[: ]/g, '-') // sv-SE = "2025-04-24 18:33:15"
+}
+
 function saveContents(){
   const textContent = textArea.value;
-  const blob = new Blob([textContent], { type: 'text/markdown' });
-  const url = URL.createObjectURL(blob);
 
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "notes.md";
-  a.click();
+  if (!(textContent.trim() === '')){
+    const blob = new Blob([textContent], { type: 'text/markdown' });
+    const url = URL.createObjectURL(blob);
+  
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `${getReadableTimestamp()}.md`;
+    a.click();
 
-  URL.revokeObjectURL(url);
+    URL.revokeObjectURL(url);
+  }
+  else {
+    alert("The document is empty... try writing something down.");
+  }
 }
